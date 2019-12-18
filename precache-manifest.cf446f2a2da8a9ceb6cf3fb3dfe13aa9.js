3eebbfdf051de34edc81788f10c7afd9894e77d4
gagharutyunyan <gagharut@gmail.com>
2019-12-18 18:29:41 +0300

    Updates

---
+++ precache-manifest.cf446f2a2da8a9ceb6cf3fb3dfe13aa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c07f0cfea5f43b6472024a489a7542d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "00610b09ccaddae8b59b",
+    "url": "/portfolio/static/css/main.82604004.chunk.css"
+  },
+  {
+    "revision": "07b91cb073f4a8f855a7",
+    "url": "/portfolio/static/js/2.2fa32626.chunk.js"
+  },
+  {
+    "revision": "00610b09ccaddae8b59b",
+    "url": "/portfolio/static/js/main.a5b79871.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
