d7ac1a77be5a39962e532e43e63d466374c3c84a
gagharutyunyan <gagharut@gmail.com>
2019-12-20 12:30:05 +0300

    Updates

--- precache-manifest.a721bc117591dd3af51c533fd4cfe7b9.js
+++ precache-manifest.a721bc117591dd3af51c533fd4cfe7b9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "175ab5d10e6aa083e7382b3a9d8217a7",
+    "revision": "b5de63bc027f866a413f0c9e57c9457a",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "adb32212e3662a43e9dc",
-    "url": "/portfolio/static/css/main.e2a4d4da.chunk.css"
+    "revision": "5fb42159e7ee89946504",
+    "url": "/portfolio/static/css/main.fee98450.chunk.css"
   },
   {
     "revision": "1ee949e5e561096b49f8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.f6ad3d00.chunk.js.LICENSE"
   },
   {
-    "revision": "adb32212e3662a43e9dc",
-    "url": "/portfolio/static/js/main.25c03235.chunk.js"
+    "revision": "5fb42159e7ee89946504",
+    "url": "/portfolio/static/js/main.36dec137.chunk.js"
   },
   {
     "revision": "551d7cedcf293c8428cc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f7ba470b71a320b99399af115205d344.js"
+  "/portfolio/precache-manifest.a721bc117591dd3af51c533fd4cfe7b9.js"
 );
 
 self.addEventListener('message', (event) => {
