6f3706338e398a13aca0ff6498992ba3ebda39d9
gagharutyunyan <gagharut@gmail.com>
2020-01-26 01:17:22 +0300

    Updates

---
+++ precache-manifest.c3d3ce35f9597959760d3155a9737b6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc22ed6839961b9ba254d8ce4ab8aeee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d436ed1128d86b59a5e6",
+    "url": "/portfolio/static/css/main.7843893d.chunk.css"
+  },
+  {
+    "revision": "9bac586c8b878c858f44",
+    "url": "/portfolio/static/js/2.7bf81def.chunk.js"
+  },
+  {
+    "revision": "9be533d47e0b58ee0373fb8c6f1e30bd",
+    "url": "/portfolio/static/js/2.7bf81def.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d436ed1128d86b59a5e6",
+    "url": "/portfolio/static/js/main.2031ba35.chunk.js"
+  },
+  {
+    "revision": "debb28456858994a1826",
+    "url": "/portfolio/static/js/runtime-main.3e206433.js"
+  }
+]);
